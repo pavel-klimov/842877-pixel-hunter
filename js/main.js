58afb6bedf52a293130783268a1fba25a7a07dc5
Pavel Klimov <pavelklimovmail@yandex.ru>
2018-11-14 02:05:04 +0300

    Создал класс для переключения экранов

---
+++ js/main.js
@@ -0,0 +1,77 @@
+'use strict';
+
+(function () {
+  const PAGE_TEMPLATE_ID_ARRAY = [`intro`, `greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`, `modal-error`, `modal-confirm`];
+  const LEFT_KEYCODE = 37;
+  const RIGHT_KEYCODE = 39;
+  const wrap = (it) => {
+    const shadow = document.createElement(`div`);
+    const content = it.content.cloneNode(true);
+    shadow.appendChild(content);
+    return shadow.cloneNode(true);
+  };
+  let Screen = class {
+    constructor() {
+      this._correntPage = 0;
+      this._numberOfScreens = PAGE_TEMPLATE_ID_ARRAY.length - 1;
+      this._templateNode = PAGE_TEMPLATE_ID_ARRAY.map((name) => {
+        return wrap(document.querySelector(`#${name}`));
+      });
+      document.addEventListener(`keydown`, (evt) => {
+        if (evt.keyCode === LEFT_KEYCODE) {
+          this.previousPage();
+        }
+        if (evt.keyCode === RIGHT_KEYCODE) {
+          this.nextPage();
+        }
+      });
+      this.changeContent(this._correntPage);
+    }
+    changeContent(number) {
+      if (number > this._numberOfScreens) {
+        this._correntPage = this._numberOfScreens;
+      } else if (number < 0) {
+        this._correntPage = 0;
+      } else {
+        this._correntPage = number;
+      }
+      document.querySelector(`#main`).innerHTML = ``;
+      document.querySelector(`#main`).appendChild(this._templateNode[this._correntPage].cloneNode(true));
+    }
+    nextPage() {
+      this._correntPage++;
+      this.changeContent(this._correntPage);
+    }
+    previousPage() {
+      this._correntPage--;
+      this.changeContent(this._correntPage);
+    }
+  };
+  let game = new Screen();
+  let arrowsElement = document.createElement(`div`);
+  arrowsElement.innerHTML = `
+    <style>
+      .arrows__wrap {
+        position: absolute;
+        top: 95px;
+        left: 50%;
+        margin-left: -56px;
+      }
+      .arrows__btn {
+        background: none;
+        border: 2px solid black;
+        padding: 5px 20px;
+      }
+    </style>
+    <button class="arrows__btn"><-</button>
+    <button class="arrows__btn">-></button>
+  `;
+  arrowsElement.classList.add(`arrows__wrap`);
+  arrowsElement.querySelector(`.arrows__btn:first-of-type`).addEventListener(`click`, () => {
+    game.previousPage();
+  });
+  arrowsElement.querySelector(`.arrows__btn:last-of-type`).addEventListener(`click`, () => {
+    game.nextPage();
+  });
+  document.body.appendChild(arrowsElement);
+})();
