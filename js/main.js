ea1e2acb6b7e9e3c03b7aa5101c78ea494fdc637
Pavel Klimov <pavelklimovmail@yandex.ru>
2018-12-10 01:32:44 +0300

    Удалил закомментированный код, пофиксил подсчёт статистики

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// import GameModel from './model/game';
-// import mainApp from './application-controller';
 import Application from './application';
 
 Application.showLoading();
-// const game = new GameModel();
-// mainApp(game);
