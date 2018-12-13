d28edc60246e9800f66a07cbc6d05e9ff8b83e36
Pavel <pavelklimovmail@yandex.ru>
2018-12-13 17:43:21 +0300

    Убрал собитие с интро кнопки

--- js/presenter/intro.js
+++ js/presenter/intro.js
@@ -1,12 +1,8 @@
 import IntroView from '../view/intro';
-import Application from '../application';
 
 export default class IntroScreen {
   constructor() {
     this._introView = new IntroView();
-    this._introView.onAsterickClick = () => {
-      Application.showWelcome();
-    };
   }
   get element() {
     return this._introView.element;
